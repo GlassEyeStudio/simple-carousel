00f32470d91a659575da17f6cb6856e87cb3e61f
GlassEyeStudio <dawid@witulski.org>
2020-09-23 17:50:18 +0200

    Added: vue-agile carousel package

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import VueAgile from "vue-agile";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
+Vue.use(VueAgile);
 
 new Vue({
   render: h => h(App)
